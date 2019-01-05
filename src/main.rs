40b9a3515f83039ffe1e00c30ee1cb34d9f370bd
LambdaXymox <lambda.xymox@gmail.com>
2019-01-05 02:53:55 -0600

    Initial code to show that one can load the library and render a font file with it.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate freetype;
+
+
 fn main() {
-    println!("Hello, world!");
+    use freetype::Library;
+
+    // Init the library
+    let lib = Library::init().unwrap();
+    // Load a font face
+    let face = lib.new_face("assets/FreeMono.ttf", 0).unwrap();
+    // Set the font size
+    face.set_char_size(40 * 64, 0, 50, 0).unwrap();
+    // Load a character
+    face.load_char('A' as usize, freetype::face::LoadFlag::RENDER).unwrap();
+    // Get the glyph instance
+    let glyph = face.glyph();
 }
