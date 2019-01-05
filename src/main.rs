bd2979f6dc6527e4a24e8c73e93915a707779abb
LambdaXymox <lambda.xymox@gmail.com>
2019-01-05 03:18:00 -0600

    Add error check for whether the font file was found.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use freetype::Library;
 use std::process;
 
 
+const FONT_FILE: &str = "assets/FreeMono.ttf";
+
+
 fn main() {
     // Init the library
     let ft = match Library::init() {
@@ -14,7 +17,13 @@ fn main() {
         }
     };
     // Load a font face
-    let face = ft.new_face("assets/FreeMono.ttf", 0).unwrap();
+    let face = match ft.new_face(FONT_FILE, 0) {
+        Ok(val) => val,
+        Err(_) => {
+            eprintln!("Could not open font file.");
+            process::exit(1);
+        }
+    };
     // Set the font size
     face.set_char_size(40 * 64, 0, 50, 0).unwrap();
     // Load a character
