4881231d9ade0ae49b70477935a5870d736bb9a5
LambdaXymox <lambda.xymox@gmail.com>
2019-01-05 03:11:35 -0600

    Add error check in main for whether we successfully loaded the FreeType library.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 extern crate freetype;
 
+use freetype::Library;
+use std::process;
+
 
 fn main() {
-    use freetype::Library;
-
     // Init the library
-    let lib = Library::init().unwrap();
+    let ft = match Library::init() {
+        Ok(val) => val,
+        Err(_) => {
+            eprintln!("Failed to initialize FreeType library.");
+            process::exit(1);
+        }
+    };
     // Load a font face
-    let face = lib.new_face("assets/FreeMono.ttf", 0).unwrap();
+    let face = ft.new_face("assets/FreeMono.ttf", 0).unwrap();
     // Set the font size
     face.set_char_size(40 * 64, 0, 50, 0).unwrap();
     // Load a character
