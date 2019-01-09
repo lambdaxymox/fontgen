ed3f0dca6f444f0c295b4502ec61844b63d16f1a
LambdaXymox <lambda.xymox@gmail.com>
2019-01-09 02:47:19 -0600

    Add end to end test.

---
+++ tests/fontgen.rs
@@ -0,0 +1,35 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::fs;
+use std::process::Command;
+use std::path::Path;
+
+
+///
+/// Generate a font sheet from a TrueType font. The font sheet and its
+/// corresponding metadata file should appear in the root directory of the source tree.
+///
+#[test]
+fn generate_a_font_sheet_from_a_ttf_file() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::main_binary()?;
+    cmd.arg("--input")
+        .arg("assets/FreeMono.ttf")
+        .arg("--output")
+        .arg("FontMono.png")
+        .arg("--padding")
+        .arg("6")
+        .arg("--slot-glyph-size")
+        .arg("128");
+    cmd.assert().success();
+
+    let file_path = Path::new("FontMono.png");
+    let meta_path = Path::new("FontMono.meta");
+
+    assert!(file_path.exists());
+    assert!(meta_path.exists());
+
+    fs::remove_file(file_path)?;
+    fs::remove_file(meta_path)?;
+
+    Ok(())
+}
